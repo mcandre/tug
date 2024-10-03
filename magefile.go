224c193efed7ecef99d851ebd1e5c185fb53d8b7
Andrew Pennebaker <andrew.pennebaker@gmail.com>
2024-10-03 00:03:29 -0500

    golint

--- magefile.go
+++ magefile.go
@@ -52,6 +52,9 @@ func GoFmt() error { return mageextras.GoFmt("-s", "-w") }
 // GoImports runs goimports.
 func GoImports() error { return mageextras.GoImports("-w") }
 
+// GoLint runs golint.
+func GoLint() error { return mageextras.GoLint() }
+
 // GoVet runs default go vet analyzers.
 func GoVet() error { return mageextras.GoVet() }
 
@@ -84,6 +87,7 @@ func Lint() error {
 	mg.Deps(GoVet)
 	mg.Deps(GoFmt)
 	mg.Deps(GoImports)
+	mg.Deps(GoLint)
 	mg.Deps(Errcheck)
 	mg.Deps(Nakedret)
 	mg.Deps(Shadow)
